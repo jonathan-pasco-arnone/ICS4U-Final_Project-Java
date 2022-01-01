7e112e68d91bd2688cc0edeb4dfdc7a521bf11ae
Ubuntu <ubuntu@ip-172-31-84-55.ec2.internal>
2022-01-01 20:22:57 +0000

    input update

--- stepThree.java
+++ stepThree.java
@@ -16,10 +16,10 @@ import java.util.Scanner;
 */
 final class stepThree {
 
-    private static int numFours = 0; // Number of ships that take up 4 squares - default = 1
+    private static int numFours = 1; // Number of ships that take up 4 squares - default = 1
     private static int numThrees = 0; // Number of ships that take up 3 squares - defualt = 3
     private static int numTwos = 0; // Number of ships that take up 2 squares - default = 2
-    private static int numOnes = 1; // Number of ships that take up 1 squares - default = 2
+    private static int numOnes = 0; // Number of ships that take up 1 squares - default = 2
     private static int numRows = 10; // The amount of rows in the grid - defualt = 10
     private static int numCol = 10; // The amount of columns in the grid - defualt = 10
 
@@ -33,13 +33,16 @@ final class stepThree {
     private static String BLUE = "\033[0;34m";
     private static String RED = "\033[0;31m";
     private static String YELLOW = "\033[0;33m";
+    private static String MAGENTA_BOLD = "\033[1;35m";
     private static String BOLD_WHITE = "\033[1;37m";
+    private static String CYAN_BACKGROUND = "\033[46m";
     private static String RESET = "\033[0m";
 
     private static String hit = "H";
     private static String miss = "M";
 
     private static char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private static int maxCharValue = 9;
 
     /**
     * Prevent instantiation
@@ -120,9 +123,9 @@ final class stepThree {
         Random randCol = new Random();
 
         // Picks a random column that is not higher tan the total columns
-        final int rowCoord = randRow.nextInt(numCol);
+        final int rowCoord = randRow.nextInt(numRows);
         // Picks a random row that is not higher than the total rows
-        final int columnCoord = randCol.nextInt(numRows);
+        final int columnCoord = randCol.nextInt(numCol);
 
         // Determines the alphabetical value of the random horizontal coordinate
         String horizontalCoordStr = Character.toString(alphabet[columnCoord]);
@@ -137,8 +140,8 @@ final class stepThree {
             // Sets the color to bold white
             System.out.print(BOLD_WHITE);
 
-            System.out.println("The computer chose the vertical coordinate of "
-                + rowCoord + " and the horizontal coordinate of " + horizontalCoordStr);
+            System.out.println("The computer chose ("
+                + rowCoord + ", " + horizontalCoordStr + ")");
 
             // Resets the color
             System.out.print(RESET);
@@ -179,53 +182,78 @@ final class stepThree {
     */
     public static void playerTurn(final ArrayList<ArrayList<String>> enemyGrid) {
 
-        // Some scanners to recieve inputs
-        final Scanner verticalInput = new Scanner(System.in);
-        final Scanner horizontalInput = new Scanner(System.in);
+        // Scanners to recieve inputs
+        final Scanner inputs = new Scanner(System.in);
 
         System.out.println("\n\nChose a place to shoot");
 
-        /*
-        *
-        * Row
-        *
-        */
+        System.out.println("\nSelect coordinates to attack");
+        final String bothCoords = inputs.nextLine();
 
-        // From 0 to the number of rows
-        System.out.println("Vertical coordinate (0-" + (numRows - 1) + "): ");
-        final int rowCoord = verticalInput.nextInt();
+        String rowCoordStr = "";
+        String columnCoordStr = "";
 
+        int rowCoord = -1;
+        int columnCoord = -1;
+
+        // Will go through every element in the input
+        for (int index = 0; index < bothCoords.length(); index++) {
+            // Checks every letter 
+            for (int letter = 0; letter < alphabet.length; letter++) {
+
+                /*
+                * If the current index of the inputted string is equal
+                * to the current alphabetical value
+                */
+                if ((String.valueOf(bothCoords.charAt(index)).equals(Character.toString(alphabet[letter])))) {
+                    columnCoordStr += Integer.toString(letter);
+                }
+            }
+
+            // Checks from number 0 to 9
+            for (int row = 0; row <= maxCharValue; row++) {
+
+                /*
+                * Checks if the current index is the current row
+                */
+                if (String.valueOf(bothCoords.charAt(index)).equals(Integer.toString(row))) {
+
+                    /*
+                    * Will "add" the current index to the row coord.
+                    * This will allow inputs like "12D" to be 12 as
+                    * apose to adding the 1 and 2 together
+                    */
+                    rowCoordStr += bothCoords.charAt(index);
+                }
+            }
+        }
+
+        if (columnCoordStr.length() >= 1) {
+            columnCoord = Integer.parseInt(columnCoordStr);
+        } else {
+            System.out.println("\nPlease input a column value.\n\n");
+            throw new java.util.InputMismatchException();
+        }
+
+        if (rowCoordStr.length() >= 1) {
+            rowCoord = Integer.parseInt(rowCoordStr);
+        } else {
+            System.out.println("\nPlease input a rows value.\n\n");
+            throw new java.util.InputMismatchException();
+        }
+
+        // If the row is out of bounds or is never assigned
         if (rowCoord > (numRows - 1) || rowCoord < 0) {
             System.out.println("\nPlease input from the "
-                + "previously mentioned vertical range.\n\n");
+                + "rows available.\n\n");
             throw new java.util.InputMismatchException();
 
         }
 
-        /*
-        *
-        * Column
-        *
-        */
-
-        int columnCoord = -1;
-
-        // From A to the number of columns
-        System.out.print("Horzontal coordinate (A-" + Character.toString(alphabet[numCol - 1]) + "): ");
-        final String horizontalCoordInput = horizontalInput.nextLine();
-
-        // Assigns the column a value
-        for (int counter = 0; counter < numCol; counter++) {
-            if (Character.toString(alphabet[counter]).equals(horizontalCoordInput)) {
-                columnCoord = counter;
-                break;
-            }
-        }
-
-        // If the column is never assigned a value, then it is invalid
-        if (columnCoord < 0) {
+        // If the column is never assigned a value or it is out of bounds
+        if (columnCoord < 0 || columnCoord > (numCol - 1)) {
             System.out.println("\nPlease input from the "
-                + "previously mentioned horizontal range.\n\n");
+                + "columns available.\n\n");
             throw new java.util.InputMismatchException();
         }
 
@@ -258,6 +286,16 @@ final class stepThree {
             throw new java.util.InputMismatchException();
         }
 
+        System.out.println("You chose (" + rowCoord + ", " + alphabet[columnCoord] + ")");
+
+        try {
+            /*
+            * Waits 1 second so the
+            * player can see if you hit or missed.
+            */
+            Thread.currentThread().sleep(1000);
+        } catch(InterruptedException ex) {
+        }
     }
 
     public static String getLetter(int number) {
@@ -303,17 +341,49 @@ final class stepThree {
                 */
                 enemyTurn(playerGrid);                
 
+                /*
+                * Waits 2 second so the
+                * player can see what the enemy has done
+                */
+                Thread.currentThread().sleep(2000);
+
                 final String currentResult = gameResult(playerGrid, enemyGrid);
 
-                // Checks a function to see if the game is over or not
+                /*
+                * Checks a function to see if the game is over.
+                * If so, who wins? If not, then continue.
+                */
                 if (currentResult.equals("player win")) {
                     gameOn = false;
+
+                    System.out.println("\n\n");
+                    System.out.print(MAGENTA_BOLD);
+                    System.out.print(CYAN_BACKGROUND);
+                    System.out.print("YOU WIN!!");
+                    System.out.print(RESET);
+                    System.out.println();
+
                     break;
                 } else if (currentResult.equals("enemy win")) {
                     gameOn = false;
+
+                    System.out.println("\n\n");
+                    System.out.print(MAGENTA_BOLD);
+                    System.out.print(CYAN_BACKGROUND);
+                    System.out.print(RESET);
+                    System.out.println();
+
                     break;
                 } else if (currentResult.equals("tie")) {
                     gameOn = false;
+
+                    System.out.println("\n\n");
+                    System.out.print(MAGENTA_BOLD);
+                    System.out.print(CYAN_BACKGROUND);
+                    System.out.print("You tie.");
+                    System.out.print(RESET);
+                    System.out.println();
+
                     break;
                 }
 
@@ -324,6 +394,7 @@ final class stepThree {
                 printEnemyGrid(enemyGrid);
 
             } catch (java.util.InputMismatchException ex) {
+            } catch (InterruptedException ex) {
             }
 
 
@@ -351,6 +422,14 @@ final class stepThree {
             // Resets the color
             System.out.print(RESET);
             System.out.print(row + " ");
+
+            /*
+            * Determines how many spaces to leave between the row
+            * value and the grid
+            */
+            for (int counter = 0; counter < (String.valueOf(numRows - 1).length() - String.valueOf(row).length()); counter++) {
+                System.out.print(" ");
+            }
 
             grid.add(new ArrayList<String>());
 
@@ -399,6 +478,17 @@ final class stepThree {
             System.out.print(RESET); 
             // Prints all the letters across the side
             System.out.print(row + " ");
+
+            /*
+            * Determines how many spaces to leave between the row
+            * value and the grid
+            */
+            for (int counter = 0; counter < (String.valueOf(
+                numRows - 1).length() - String.valueOf(row).length());
+                counter++) {
+
+                System.out.print(" ");
+            }
 
             for (int column = 0; column < numCol; column++) {
                 int currentIndexNumber = 0;
@@ -662,6 +752,8 @@ final class stepThree {
     */
     public static void main(final String[] args) {
         System.out.println("This is the Battleship program\n");
+
+        System.out.println("\007");
 
         if (numCol > 25) {
             System.out.println("There are too many columns. Please make it between 1 and 25");
